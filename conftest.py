807554cb19e3633d35e0775bb6a4a021975682b2
Utsav247-crypto <57640293+Utsav247-crypto@users.noreply.github.com>
2023-02-27 01:17:40 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,10 @@
 import pytest
 @pytest.fixture(autouse=True)
-def setup():
-    print('login browser')
-    yield
-    print('close browser')
-    
 def setup(request):
+    print('login browser')
     session = request.node
     html = request.config._html
     report_content = html._generate_report(session)
     html._save_report(report_content)
+    yield
+    print('close browser')
