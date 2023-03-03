7301f480727620416f1623a80a59464a51e620f8
Utsav247-crypto <57640293+Utsav247-crypto@users.noreply.github.com>
2023-03-03 14:47:02 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,5 +10,5 @@ def setup():
 def pytest_terminal_summary(terminalreporter):
     print(terminalreporter.config)
     if not hasattr(terminalreporter.config, 'workerinput'):
-        client = WebClient(token='xoxb-4891217860754-4891249655875-8Id1BqTvJY1mQGzuK2uppTQE')
-        client.chat_postMessage(channel="automation",text="My File")
+        client = WebClient(token='xoxb-4891217860754-4891249655875-tDE54Ju4P5RDnbtnMcuLnirH')
+        client.chat_postMessage(channel="random",text="My File")
