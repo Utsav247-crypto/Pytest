b6610c02bbfa9889ac3f3bc8212fa7d01777b93d
Utsav247-crypto <57640293+Utsav247-crypto@users.noreply.github.com>
2023-03-03 14:36:22 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,14 @@
 import pytest
+from slack_sdk import WebClient
+
 @pytest.fixture(scope='function',autouse=True)
 def setup():
     print('login browser')
     yield
     print('close browser')
+
+def pytest_terminal_summary(terminalreporter):
+    print(terminalreporter.config)
+    if not hasattr(terminalreporter.config, 'workerinput'):
+        client = WebClient(token='xoxb-4891217860754-4891249655875-8Id1BqTvJY1mQGzuK2uppTQE')
+        client.chat_postMessage(channel="automation",text="My File")
